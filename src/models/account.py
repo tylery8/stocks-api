bf56ba31870a3d7d5ccd52ef236ea5ed9f438afb
tylery8 <tylertfc99@gmail.com>
2021-04-29 13:00:21 -0400

    bug fix

--- src/models/account.py
+++ src/models/account.py
@@ -1,5 +1,5 @@
 class Account:
     def __init__(self, account_id, apikey=None, watchlist=None):
         self.account_id = account_id
-        self.api_key = apikey
+        self.apikey = apikey
         self.watchlist = [] if watchlist is None else watchlist
