a278f8f82c0a4de7dbc08572c22ac8e27130da84
tylery8 <tylertfc99@gmail.com>
2021-04-29 12:11:16 -0400

    added apikey

--- src/functions/accounts.py
+++ src/functions/accounts.py
@@ -27,3 +27,19 @@ def update_watchlist(account_id, watchlist):
         ACCOUNTS_CLIENT.put_item(account)
 
     return 200, watchlist
+
+
+def read_apikey(account_id):
+    account = ACCOUNTS_CLIENT.get_item(account_id)
+
+    return 200, account.apikey
+
+
+def update_apikey(account_id, apikey):
+    account = ACCOUNTS_CLIENT.get_item(account_id)
+
+    if account.apikey != apikey:
+        account.apikey = apikey
+        ACCOUNTS_CLIENT.put_item(account)
+
+    return 200, apikey
--- src/models/account.py
+++ src/models/account.py
@@ -1,4 +1,5 @@
 class Account:
-    def __init__(self, account_id, watchlist=None):
+    def __init__(self, account_id, apikey=None, watchlist=None):
         self.account_id = account_id
+        self.api_key = apikey
         self.watchlist = [] if watchlist is None else watchlist
