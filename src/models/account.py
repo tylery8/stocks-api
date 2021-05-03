6d5e3530bb5b0f83ed27f6e1cc9ec5ad7a66eee4
tylery8 <tylertfc99@gmail.com>
2021-05-03 15:12:07 -0400

    bug fix

--- src/models/account.py
+++ src/models/account.py
@@ -2,4 +2,4 @@ class Account:
     def __init__(self, account_id, watchlist=None, portfolio=None):
         self.account_id = account_id
         self.watchlist = [] if watchlist is None else watchlist
-        self.portfolio = {'deposit': 0, 'trades': []} if portfolio is None else portfolio
+        self.portfolio = {'deposit': 0, 'cash': 0, 'trades': []} if portfolio is None else portfolio
