d1c6120b1526c524ba587b6973739aa9c71f1e50
tylery8 <tylertfc99@gmail.com>
2021-05-06 16:33:16 -0400

    limit total deposits to 1M

--- src/functions/accounts.py
+++ src/functions/accounts.py
@@ -110,6 +110,9 @@ def add_deposit(account_id, amount):
 
     account = ACCOUNTS_CLIENT.get_item(account_id)
 
+    if account.portfolio['deposit'] + amount >= 1000000:
+        raise IllegalAmountException("Total deposits must be less than $1M")
+
     if round(now() * 1000) - account.portfolio['last_action'] <= 5000:
         raise RapidTradeException('Please wait at least 5 seconds between actions')
 
